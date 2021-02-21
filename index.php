c8ed482ac48e798593c09244c9f66d6acb08c490
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-02-21 23:48:18 +0600

    Buttons and progress bar added for controlling music.

--- index.php
+++ index.php
@@ -13,9 +13,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
     <title>Welcome to Spotify!</title>
 </head>
 <body>
-    <p> Hello! </p>
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <buttons class="controlButton shuffle" title="Shuffle">
+                            <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                        </buttons>
+                        <buttons class="controlButton previous" title="Previous">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </buttons>
+                        <buttons class="controlButton play" title="Play">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </buttons>
+                        <buttons class="controlButton pause" title="Pause" style="display: none;">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </buttons>
+                        <buttons class="controlButton next" title="Next">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </buttons>
+                        <buttons class="controlButton repeat" title="Repeat">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </buttons>
+                    </div>
+                    <div class="playBackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBackground">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
+                </div>
+            </div>
+            <div id="nowPlayingRight">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
