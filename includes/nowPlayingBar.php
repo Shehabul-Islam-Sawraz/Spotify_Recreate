8c89c861395497167f1dce652493534195551285
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-04-13 22:10:44 +0600

    Redirecting to album page and artist page from Now Playing Bar.

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -143,10 +143,13 @@
             $.post("includes/handler/ajax/getArtistJSON.php", {artistId:track.artist} , function(data){
                 var artist = JSON.parse(data);
                 $(".artistName span").text(artist.name);
+                $(".artistName span").attr("onclick","openPage('artist.php?id="+artist.id+"')");
             });
             $.post("includes/handler/ajax/getAlbumJSON.php", {albumId:track.album} , function(data){
                 var album = JSON.parse(data);
                 $(".albumLink img").attr("src",album.artworkPath);
+                $(".albumLink img").attr("onclick","openPage('album.php?id="+album.id+"')");
+                $(".trackName span").attr("onclick","openPage('album.php?id="+album.id+"')");
             });
             audioElement.setTrack(track);
             //audioElement.audio.currentTime =0;
